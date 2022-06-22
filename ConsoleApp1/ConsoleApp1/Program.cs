bf0d94d4c09cd41e05c5a6149b47fb48fa700981
Samuel Gosaye <samuelgosaye02@gmail.com>
2022-06-22 15:15:40 +0300

    Guessing number

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int luckyNum=2;
+            int guess;
+            bool Continue = true;
+            int guessChance = 3;
+
+            do
+            {
+                
+                Console.WriteLine("Guess the Lucky number: ");
+                guess = int.Parse(Console.ReadLine());
+                
+
+                if (guess == luckyNum)
+                {
+                    Console.WriteLine("You Won!");
+                    Continue = false;
+                    
+                }
+                else
+                {
+                    Console.WriteLine("You Lost!");
+                    
+                    guessChance--;
+                    if(guessChance == 0)
+                    {
+                        Console.WriteLine("Out of Chance!!");
+                        Continue = false;
+                        
+                    }
+                }
+
+
+                Console.ReadKey();
+
+            } while (Continue);
+                
+
+            
+        }
+    }
+}
